1df52889ffd24f383823e95da75ae5efbfa585a5
Kerhoff <kerhoffsky@gmail.com>
2022-07-06 22:05:02 +0300

    Rename master to main

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="",
+    port="",
+    password="",
+    decode_responses=True
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/products")
+def all():
+    return [format(pk) for pk in Product.all_pks()]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        "id": product.pk,
+        "name": product.name,
+        "price": product.price,
+        "quantity": product.quantity
+    }
+
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
+
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return Product.delete(pk)
